a8d4d43737421532904dc3f627a66fc3f619c7e6
Lerg <sergey.lerg@gmail.com>
2016-10-26 06:40:23 +0500

    Major plugin update.

--- main.lua
+++ main.lua
@@ -1,172 +1,253 @@
+-- Sample app for the TextToSpeech plugin for Corona SDK
+-- Documentation: http://spiralcodestudio.com/plugin-texttospeech/
+
 display.setStatusBar(display.HiddenStatusBar)
 
 local json = require('json')
 local widget = require('widget')
+local audio = require('audio')
 local texttospeech = require('plugin.texttospeech')
-
---[[
-texttospeech.init(callback)
-
-Must be callled before using any other Text To Speech functions.
-
-callback(event) - called on initialization complete.
-    event.isError - boolean, true if there was any error.
-    event.engines - table, a list of available engines. Key - engine label, value - engine package name. Requires Android 4.0+.
-    event.defaultEngine - string, package name of the default engine. Android only.
-
-
-texttospeech.getLanguagesAndVoices(), table
-
-Returns a list of available languages and voices for current engine. On Android requires Android 5.0+.
-
-table.languages - array of strings, a list of languages.
-table.voices - table, a list of voices. Key - voice name, value - voice language. On iOS there can be iether one or two voices: 'default' and 'alex'. Alex is Siri's voice and only available on iOS 9+.
-
-
-texttospeech.setEngine(engine), boolean
-
-Sets an engine to use. Returns true on success. Engine might require some time to be initialized. Android only.
-
-engine - string, engine package name.
-
-
-texttospeech.speak(text, [options])
-
-Generate and play the speech.
-
-text - string, text to be spoken.
-options.language - string, language to be used, like 'ru-RU', 'fr-FR'. Default is 'en-US'.
-options.voice - string, voice to be used. Requires Android 5.0+.
-options.pitch - float, alter the pitch of the voice, value from 0.5 to 2.0. Default is 1.0.
-options.rate - float, adjust speaking speed. Android value from 0.5 to 2.0, default is 1.0. iOS value - no idea, it's a mess.
-options.volume - float, set volume of the voice, value from 0.0 to 1.0. Default is 1.0. On Android requires Android 3.0+.
-option.id - string, optional id for the text, passed to callbacks. If not supplied - a random value.
-options.onStart(id) - function, called when speech has started. On Android requires Android 4.0.3+.
-options.onProgress(id, start, count) - function, called when speech portion is about to be spoken. start and count indicate the portion of the original UTF-8 text. iOS only.
-options.onComplete(id) - function, called when speech has ended.
-
-
-texttospeech.isSpeaking(), boolean
-
-Returns true if currently speaking.
-
-
-texttospeech.stop()
-
-Stop the speech.
-
-]]
 
 local platform = system.getInfo('platformName')
 local apiLevel = system.getInfo('androidApiLevel')
 -- 14 -> Android 4.0
 -- 21 -> Android 5.0
+texttospeech.enableDebug()
 
-texttospeech.init(function(event)
-    print('Init callback')
-    if not event.isError then
-        if platform == 'Android' then
-            if apiLevel >= 14 then
-                print('Engines:', json.prettify(event.engines))
-            end
-            print('Default engine:', event.defaultEngine)
-        end
+local function onInit(event)
+	print('Init callback')
+	if not event.isError then
+		print(platform)
+		if platform == 'Android' then
+			if apiLevel >= 14 then
+				print('Engines:', json.prettify(event.engines))
+			end
+			print('Default engine:', event.defaultEngine)
+		end
 
-        if platform == 'iPhone OS' or (platform == 'Android' and apiLevel >= 21) then
-            local languagesAndVoices = texttospeech.getLanguagesAndVoices()
-            if languagesAndVoices then
-                print('Languages and voices:', json.prettify(languagesAndVoices))
-            end
-        end
-    else
-        print('Error initializing Text To Speech')
-    end
-end)
+		if platform == 'iPhone OS' or platform == 'Mac OS X' or (platform == 'Android' and apiLevel >= 21) then
+			local languagesAndVoices = texttospeech.getLanguagesAndVoices()
+			if languagesAndVoices then
+				print('Languages and voices:', json.prettify(languagesAndVoices))
+			end
+		end
+	else
+		print('Error initializing Text To Speech')
+	end
+end
+texttospeech.init(onInit)
+
+local voice
+if platform == 'Mac OS X' then
+	voice = 'com.apple.speech.synthesis.voice.samantha'
+elseif platform == 'iPhone OS' then
+	voice = 'alex'
+end
 
 display.setDefault('background', 1)
 
-local x, y = display.contentCenterX, display.contentCenterY
-local w, h = display.contentWidth * 0.8, 50
+local xl, xr, y = display.contentWidth * .25, display.contentWidth * .75, display.contentCenterY
+local w, h = display.contentWidth * 0.45, 50
 
 widget.newButton{
-    x = x, y = y - 200,
-    width = w, height = h,
-    label = 'Speak',
-    onRelease = function()
-        local text = 'Nobody expects the Spanish Inquisition! Our chief weapon is surprise, surprise and fear, fear and surprise, our two weapons are fear and surprise, and ruthless efficiency.'
-        texttospeech.speak(text, {
-            --voice = 'alex', -- iOS 9.0+
-            onStart = function(id)
-                print('Speech "' .. id .. '" has started.')
-            end,
-            onProgress = function(id, start, count)
-                print('Speech "' .. id .. '" progress: ' .. text:sub(start, start + count))
-            end,
-            onComplete = function(id)
-                print('Speech "' .. id .. '" has ended.')
-            end
-        })
-    end}
+	x = xl, y = y - 200,
+	width = w, height = h,
+	label = 'Speak',
+	onRelease = function()
+		local text = 'Nobody expects the Spanish Inquisition! Our chief weapon is surprise, surprise and fear, fear and surprise, our two weapons are fear and surprise, and ruthless efficiency.'
+		texttospeech.speak(text, {
+			voice = voice,
+			onStart = function(event)
+				print('Speech "' .. event.id .. '" has started.')
+			end,
+			onProgress = function(event)
+				if event.start then
+					print('Speech "' .. event.id .. '" progress: ' .. text:sub(event.start, event.start + event.count))
+				elseif event.audio then
+					print('Speech "' .. event.id .. '" progress: chunk length - ' .. #event.audio)
+				end
+			end,
+			onComplete = function(event)
+				print('Speech "' .. event.id .. '" has ended.')
+			end
+		})
+	end}
 
 widget.newButton{
-    x = x, y = y - 120,
-    width = w, height = h,
-    label = 'Speak Russian',
-    onRelease = function()
-        local text = 'Ну и гадость же эта ваша заливная рыба.'
-        texttospeech.speak(text, {
-            language = 'ru-RU',
-            --voice = 'ru-RU-locale', -- Optional. Has effect only on Android 5.0+
-            pitch = 0.8,
-            rate = 0.6,
-            volume = 0.9,
-            id = 'fish',
-            onStart = function(id)
-                print('Speech "' .. id .. '" has started.')
-            end,
-            onProgress = function(id, start, count)
-                -- Simple string.sub() won't work for UTF-8 strings, use UTF-8 plugin
-                print('Speech "' .. id .. '" progress: start - ' .. start .. ', count - ' .. count)
-            end,
-            onComplete = function(id)
-                print('Speech "' .. id .. '" has ended.')
-            end
-        })
-    end}
+	x = xr, y = y - 200,
+	width = w, height = h,
+	label = 'Speak Russian',
+	onRelease = function()
+		local text = 'Ну и гадость же эта ваша заливная рыба.'
+		texttospeech.speak(text, {
+			language = 'ru-RU',
+			--voice = 'ru-RU-locale', -- Optional. Has effect only on Android 5.0+
+			pitch = 0.8,
+			rate = 0.6,
+			volume = 0.9,
+			id = 'fish',
+			onStart = function(event)
+				print('Speech "' .. event.id .. '" has started.')
+			end,
+			onProgress = function(event)
+				-- Simple string.sub() won't work for UTF-8 strings, use UTF-8 plugin
+				if event.start then
+					print('Speech "' .. event.id .. '" progress: start - ' .. event.start .. ', count - ' .. event.count)
+				elseif event.audio then
+					print('Speech "' .. event.id .. '" progress: chunk length - ' .. #event.audio)
+				end
+			end,
+			onComplete = function(event)
+				print('Speech "' .. event.id .. '" has ended.')
+			end
+		})
+	end}
 
 widget.newButton{
-    x = x, y = y - 40,
-    width = w, height = h,
-    label = 'Is speaking?',
-    onRelease = function()
-        native.showAlert('Is speaking?', texttospeech.isSpeaking() and 'Yes' or 'No', {'OK'})
-    end}
+	x = xl, y = y - 120,
+	width = w, height = h,
+	label = 'Speak Japanese',
+	onRelease = function()
+		local text = '愛と正義の、セーラー服美少女戦士、セーラームーン! 月に代わって、お仕置きよ!'
+		texttospeech.speak(text, {
+			language = 'ja-JP',
+			pitch = 1.2,
+			rate = 1,
+			id = 'japanese',
+			onStart = function(event)
+				print('Speech "' .. event.id .. '" has started.')
+			end,
+			onProgress = function(event)
+				-- Simple string.sub() won't work for UTF-8 strings, use UTF-8 plugin
+				if event.start then
+					print('Speech "' .. event.id .. '" progress: start - ' .. event.start .. ', count - ' .. event.count)
+				elseif event.audio then
+					print('Speech "' .. event.id .. '" progress: chunk length - ' .. #event.audio)
+				end
+			end,
+			onComplete = function(event)
+				print('Speech "' .. event.id .. '" has ended.')
+			end,
+			onPause = function(event)
+				print('Speech "' .. event.id .. '" has been paused.')
+			end,
+			onContinue = function(event)
+				print('Speech "' .. event.id .. '" has been continnued.')
+			end
+		})
+	end}
+
+	widget.newButton{
+		x = xr, y = y - 120,
+		width = w, height = h,
+		label = 'Speak to file',
+		onRelease = function()
+			local extension = 'wav'
+			if platform == 'Mac OS X' then
+				extension = 'aiff'
+			end
+			local text = 'Spiral Code Studio presents you the Text To Speech plugin.'
+			texttospeech.speak(text, {
+				voice = voice,
+				id = 'file',
+				filename = 'speech.' .. extension,
+				baseDir = system.DocumentsDirectory,
+				onStart = function(event)
+					print('Speech "' .. event.id .. '" has started.')
+				end,
+				onProgress = function(event)
+					if event.start then
+						print('Speech "' .. event.id .. '" progress: ' .. text:sub(event.start, event.start + event.count))
+					elseif event.audio then
+						print('Speech "' .. event.id .. '" progress: chunk length - ' .. #event.audio)
+					end
+				end,
+				onComplete = function(event)
+					print('Speech "' .. event.id .. '" has ended.')
+				end,
+				onPause = function(event)
+					print('Speech "' .. event.id .. '" has been paused.')
+				end,
+				onContinue = function(event)
+					print('Speech "' .. event.id .. '" has been continnued.')
+				end
+			})
+		end}
 
 widget.newButton{
-    x = x, y = y + 40,
-    width = w, height = h,
-    label = 'Stop',
-    onRelease = function()
-        texttospeech.stop()
-    end}
+	x = xl, y = y - 40,
+	width = w, height = h,
+	label = 'Is speaking?',
+	onRelease = function()
+		native.showAlert('Is speaking?', texttospeech.isSpeaking() and 'Yes' or 'No', {'OK'})
+	end}
 
 widget.newButton{
-    x = x, y = y + 120,
-    width = w, height = h,
-    label = 'Get lang/voices',
-    onRelease = function()
-        local languagesAndVoices = texttospeech.getLanguagesAndVoices()
-        if languagesAndVoices then
-            print('Languages and voices:', json.prettify(languagesAndVoices))
-        end
-    end}
+	x = xr, y = y - 40,
+	width = w, height = h,
+	label = 'Stop',
+	onRelease = function()
+		texttospeech.stop()
+	end}
+
+widget.newButton{
+	x = xl, y = y + 40,
+	width = w, height = h,
+	label = 'Pause',
+	onRelease = function()
+		texttospeech.pause()
+	end}
+
+widget.newButton{
+	x = xr, y = y + 40,
+	width = w, height = h,
+	label = 'Continue',
+	onRelease = function()
+		texttospeech.continue()
+	end}
+
+widget.newButton{
+	x = xl, y = y + 120,
+	width = w, height = h,
+	label = 'Pause at word',
+	onRelease = function()
+		texttospeech.pause(true)
+	end}
+
+widget.newButton{
+	x = xr, y = y + 120,
+	width = w, height = h,
+	label = 'Get lang/voices',
+	onRelease = function()
+		local languagesAndVoices = texttospeech.getLanguagesAndVoices()
+		if languagesAndVoices then
+			print('Languages and voices:', json.prettify(languagesAndVoices))
+		end
+	end}
+
+widget.newButton{
+	x = xl, y = y + 200,
+	width = w, height = h,
+	label = 'Play file',
+	onRelease = function()
+		local extension = 'wav'
+		if platform == 'Mac OS X' then
+			extension = 'aiff'
+		end
+		local speech = audio.loadSound('speech.' .. extension, system.DocumentsDirectory)
+		if speech then
+			audio.play(speech, {onComplete = function()
+				audio.dispose(speech)
+			end})
+		end
+	end}
 
 if platform == 'Android' then
-    widget.newButton{
-        x = x, y = y + 200,
-        width = w, height = h,
-        label = 'Set Pico TTS',
-        onRelease = function()
-            native.showAlert('Success?', texttospeech.setEngine('com.svox.pico') and 'Yes' or 'No', {'OK'})
-        end}
+	widget.newButton{
+		x = xr, y = y + 200,
+		width = w, height = h,
+		label = 'Set Pico TTS',
+		onRelease = function()
+			texttospeech.init({engine = 'com.svox.pico', listener = onInit})
+		end}
 end
